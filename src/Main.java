b046ae2f88230baf4fbf788ff803faf96c5d6d0d
erdelyiadi <erdelyiadi@gmail.com>
2023-05-17 11:32:29 +0200

    main újboli commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello world! + hello");
+
+        Allat a1 = new Allat("Lana");
+
+
+        System.out.println(a1.nev);
+
     }
 
 
